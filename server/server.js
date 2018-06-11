f8b452fc7e57d57433af8f52388b24a6958e405c
Travis Croxall <travis@csaanimalnutrition.com>
2018-06-11 08:55:00 -0400

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const express = require('express');
+
+const publicPath = path.join(__dirname,'../public');
+var app = express();
+app.use(express.static(publicPath));
+
+const port = process.env.PORT || 3000;
+
+
+app.listen(port, () => {
+  console.log(`Started up at port ${port}`);
+});
+
+module.exports = {app};
