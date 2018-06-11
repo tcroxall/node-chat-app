fbb5f5ca7fb0cfc39ac7c36f9aa5bcc4532e52d9
Travis Croxall <travis@csaanimalnutrition.com>
2018-06-11 10:36:12 -0400

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,6 @@
 var socket = io();
 socket.on('connect', function() {
   console.log('Connected to server');
-
-  socket.emit('createMessage', {
-    from: 'from user',
-    text: 'createMessage text'
-  });
 });
 socket.on('disconnect', function() {
   console.log('Disconnect from server');
