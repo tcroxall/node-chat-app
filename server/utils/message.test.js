5d76d9d88cdc0b629e46999ef2b7dcacbe9ee846
Travis Croxall <travis@csaanimalnutrition.com>
2018-06-12 14:06:38 -0400

    Create generateMessae util

---
+++ server/utils/message.test.js
@@ -0,0 +1,15 @@
+var expect = require('expect');
+
+var {generateMessage} = require('./message.js');
+
+describe('generateMessage', () => {
+    it('should generate correct message object', () => {
+        var from = 'Jen';
+        var text = 'Some message';
+        var message = generateMessage(from, text);
+
+        expect(typeof message.createdAt).toBe('number');
+        expect(message.from).toBe(from);
+        expect(message.text).toBe(text);
+    })
+});
